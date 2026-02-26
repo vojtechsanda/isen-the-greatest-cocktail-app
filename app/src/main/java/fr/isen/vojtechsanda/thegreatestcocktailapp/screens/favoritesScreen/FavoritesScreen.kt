44f30546ceaf7debfd44c36e2c2249c2817d22a8
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2026-02-27 00:22:37 +0100

    feat: Connect favorites screen to API and SharedPreferences

--- app/src/main/java/fr/isen/vojtechsanda/thegreatestcocktailapp/screens/favoritesScreen/FavoritesScreen.kt
+++ app/src/main/java/fr/isen/vojtechsanda/thegreatestcocktailapp/screens/favoritesScreen/FavoritesScreen.kt
@@ -1,6 +1,7 @@
 package fr.isen.vojtechsanda.thegreatestcocktailapp.screens.favoritesScreen
 
 import android.content.Intent
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,75 +9,53 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import fr.isen.vojtechsanda.thegreatestcocktailapp.DrinkDetailActivity
-import fr.isen.vojtechsanda.thegreatestcocktailapp.R
-import fr.isen.vojtechsanda.thegreatestcocktailapp.models.CategoryData
+import fr.isen.vojtechsanda.thegreatestcocktailapp.dtos.toDrinkDetailData
 import fr.isen.vojtechsanda.thegreatestcocktailapp.models.DrinkDetailData
-import fr.isen.vojtechsanda.thegreatestcocktailapp.models.IngredientData
+import fr.isen.vojtechsanda.thegreatestcocktailapp.network.ApiClient
 import fr.isen.vojtechsanda.thegreatestcocktailapp.screens.favoritesScreen.views.FavoritesTopAppBar
+import fr.isen.vojtechsanda.thegreatestcocktailapp.storage.FavoritesSharedPreferencesManager
 import fr.isen.vojtechsanda.thegreatestcocktailapp.views.columnItemCard.ColumnItemCardView
 import fr.isen.vojtechsanda.thegreatestcocktailapp.views.layout.appScaffold.AppScaffold
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 
 @Composable
 fun FavoritesScreen(modifier: Modifier = Modifier, withBottomBar: Boolean = false) {
-    val drinks = listOf(
-        DrinkDetailData(
-            id = "super-cocktail",
-            imageUrl = "https://example.com/example.png",
-            name = "Super cocktail",
-            categories = listOf(
-                CategoryData(
-                    id = "other-unknown",
-                    name = "Other / Unknown",
-                    iconVector = Icons.Default.Info,
-                    backgroundFromRes = (R.color.light_blue),
-                    backgroundToRes = (R.color.dark_blue)
-                ),
-
-                CategoryData(
-                    id = "non-alcoholic",
-                    name = "Non alcoholic",
-                    iconVector = Icons.Default.Warning,
-                    backgroundFromRes = (R.color.muted_sage),
-                    backgroundToRes = (R.color.dark_forest_green)
-                ),
-
-                CategoryData(
-                    id = "highball-glass",
-                    name = "Highball glass",
-                    iconVector = Icons.Default.Star,
-                    backgroundFromRes = (R.color.transparent),
-                    backgroundToRes = (R.color.transparent),
-                    textColorRes = (R.color.teal_200),
-                )
-            ),
-
-            ingredients = listOf(
-                IngredientData(
-                    name = "Yoghurt",
-                    amount = "2 cups",
-                ),
-                IngredientData(
-                    name = "Fruit",
-                    amount = "1 piece",
-                )
-            ),
-
-            recipe = "This is some very interesting recipe on how to do the Super cocktail.",
-        )
-    )
-
     val context = LocalContext.current
+    val favoritesManager = FavoritesSharedPreferencesManager()
+    val favoriteDrinksIdsState =
+        favoritesManager.getFavoriteDrinksFlow(context).collectAsState(initial = setOf())
+
+    val drinksState = remember { mutableStateOf<List<DrinkDetailData>>(emptyList()) }
+
+    LaunchedEffect(favoriteDrinksIdsState.value) {
+        try {
+            val drinksDetails = favoriteDrinksIdsState.value.toList().map { drinkId ->
+                async {
+                    ApiClient.retrofit.getDrinkDetail(drinkId)
+                }
+            }.awaitAll()
+                .flatMap { response -> response.drinks.map { drink -> drink.toDrinkDetailData() } }
+
+            drinksState.value = drinksDetails
+        } catch (e: Exception) {
+            Log.e("FavoritesScreen", "Error fetching drinks details", e)
+        }
+    }
+
+
     val drinkDetailIntent = Intent(context, DrinkDetailActivity::class.java)
 
     AppScaffold(
@@ -89,26 +68,33 @@ fun FavoritesScreen(modifier: Modifier = Modifier, withBottomBar: Boolean = fals
                 verticalArrangement = Arrangement.spacedBy(24.dp),
                 contentPadding = PaddingValues(20.dp, 8.dp, 20.dp, 20.dp)
             ) {
-
-                items(drinks) { drink ->
-                    ColumnItemCardView(
-                        modifier = Modifier.clickable {
-                            drinkDetailIntent.putExtra("DRINK_ID", drink.id)
-                            context.startActivity(drinkDetailIntent)
-                        },
-                        title = drink.name,
-                        leading = {
-                            if (drink.imageUrl != null)
-                                AsyncImage(
-                                    model = drink.imageUrl,
-                                    contentDescription = null,
-                                    modifier = Modifier
-                                        .clip(CircleShape)
-                                        .width(48.dp)
+                if (drinksState.value.isEmpty())
+                    item {
+                        Text("No favorites yet")
+                    }
+                else
+                    items(drinksState.value) { drink ->
+                        ColumnItemCardView(
+                            modifier = Modifier.clickable {
+                                drinkDetailIntent.putExtra(
+                                    DrinkDetailActivity.EXTRA_DRINK_ID,
+                                    drink.id
                                 )
-                        }
-                    )
-                }
+                                context.startActivity(drinkDetailIntent)
+                            },
+                            title = drink.name,
+                            leading = {
+                                if (drink.imageUrl != null)
+                                    AsyncImage(
+                                        model = drink.imageUrl,
+                                        contentDescription = null,
+                                        modifier = Modifier
+                                            .clip(CircleShape)
+                                            .width(48.dp)
+                                    )
+                            }
+                        )
+                    }
             }
         })
 }
